d883b0693c51a66e6ad6e0e7dedf45a5c868e93a
Alessandro Tundo <alessandrotundo94@gmail.com>
2021-01-22 00:16:41 +0100

    Fixrun_scenario service name

--- run_scenario.sh
+++ run_scenario.sh
@@ -5,9 +5,9 @@ service_name=$2
 service_port=$3
 
 # Copy instrumentation source
-yes | cp -r "monitor/$service_name/filter" "piggymetrics/$service_name/src/main/java/com/piggymetrics/account/"
-yes | cp "monitor/$service_name/config/"*.java "piggymetrics/$service_name/src/main/java/com/piggymetrics/account/config/"
-yes | cp "monitor/$service_name/logback-spring.xml" "piggymetrics/$service_name/src/main/resources/logback-spring.xml"
+yes | cp -r "monitor/$service_name-service/filter" "piggymetrics/$service_name-service/src/main/java/com/piggymetrics/account/"
+yes | cp "monitor/$service_name-service/config/"*.java "piggymetrics/$service_name-service/src/main/java/com/piggymetrics/account/config/"
+yes | cp "monitor/$service_name-service/logback-spring.xml" "piggymetrics/$service_name-service/src/main/resources/logback-spring.xml"
 
 # Package with maven
 cd piggymetrics
@@ -18,13 +18,13 @@ cd ..
 yes | cp monitor/docker-compose.custom.yml piggymetrics/
 
 # Build services
-docker-compose -f piggymetrics/docker-compose.custom.yml build config registry auth-mongodb auth-service "$service_name" "$service_name-mongodb"
+docker-compose -f piggymetrics/docker-compose.custom.yml build config registry auth-mongodb auth-service "$service_name-service" "$service_name-mongodb"
 
 # Run services
 docker-compose -f piggymetrics/docker-compose.custom.yml up -d config registry
 docker-compose -f piggymetrics/docker-compose.custom.yml up -d auth-mongodb "$service_name-mongodb"
 sleep 5
-docker-compose -f piggymetrics/docker-compose.custom.yml up -d auth-service "$service_name"
+docker-compose -f piggymetrics/docker-compose.custom.yml up -d auth-service "$service_name-service"
 
 auth_service_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps | grep piggymetrics_auth-service | awk '{print $1}'))
 count=1
@@ -45,7 +45,7 @@ count=1
 while [ -z "$service_ip" ]
 do
 	if (( $count > 50 )); then
-		echo "No IP address found for $service_name after 50 retries"
+		echo "No IP address found for $service_name-service after 50 retries"
 		exit 1
 	else
 	    sleep 10
@@ -60,7 +60,7 @@ if [ !-z "$service_ip" ]; then
 	    printf '.'
 	    sleep 20
 	done
-	echo "Connected to $service_name"
+	echo "Connected to $service_name-service"
 fi
 
 interface=$(sudo brctl show | awk 'NF>1 && NR>1 {print $1}' | grep br-)
