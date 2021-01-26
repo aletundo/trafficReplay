21429bf29265236ee3146c0b55a435c62d525c20
Alessandro Tundo <alessandrotundo94@gmail.com>
2021-01-26 16:25:19 +0100

    Fix make dir for overhead

--- run_all_scenarios_overhead_baseline.sh
+++ run_all_scenarios_overhead_baseline.sh
@@ -5,6 +5,9 @@ runs=$1;
 mkdir -p overhead-experiments/baseline
 
 for (( i = 0; i < $runs; i++ )); do
+
+	sudo systemctl start metricbeat.service
+
 	./run_scenario.sh -s 001_scenario -svc account --latency
 	./run_scenario.sh -s 002_scenario -svc account --no-build --no-run --latency
 	./run_scenario.sh -s 003_scenario -svc account --no-build --no-run --latency
@@ -14,24 +17,26 @@ for (( i = 0; i < $runs; i++ )); do
 	./run_scenario.sh -s 007_scenario -svc account --no-build --no-run --latency
 	./run_scenario.sh -s 008_scenario -svc account --no-build --no-run --latency
 
-	mkdir overhead-experiments/baseline/account-service/run-$i
-	cp scenarios/account-service/*.log overhead-experiments/baseline/account-service/run-$i
+	mkdir -p overhead-experiments/baseline/account-service/run-$i
+	cp scenarios/account-service/*.log overhead-experiments/baseline/account-service/run-$i/
 
 	./run_scenario.sh -s 001_scenario -svc statistics --no-build --no-run --latency
 	./run_scenario.sh -s 002_scenario -svc statistics --no-build --no-run --latency
 	./run_scenario.sh -s 003_scenario -svc statistics --no-build --no-run --latency
 	./run_scenario.sh -s 004_scenario -svc statistics --no-build --no-run --latency
 
-	mkdir overhead-experiments/baseline/statistics-service/run-$i
-	cp scenarios/statistics-service/*.log overhead-experiments/baseline/statistics-service/run-$i
+	mkdir -p overhead-experiments/baseline/statistics-service/run-$i
+	cp scenarios/statistics-service/*.log overhead-experiments/baseline/statistics-service/run-$i/
 
 	./run_scenario.sh -s 001_scenario -svc notification --no-build --no-run --latency
 	./run_scenario.sh -s 002_scenario -svc notification --no-build --no-run --latency
 	./run_scenario.sh -s 003_scenario -svc notification --no-build --no-run --latency
 	./run_scenario.sh -s 004_scenario -svc notification --no-build --no-run --latency
 
-	mkdir overhead-experiments/baseline/notification-service/run-$i
-	cp scenarios/notification-service/*.log overhead-experiments/baseline/notification-service/run-$i
+	mkdir -p overhead-experiments/baseline/notification-service/run-$i
+	cp scenarios/notification-service/*.log overhead-experiments/baseline/notification-service/run-$i/
+
+	sudo systemctl stop metricbeat.service
 
 	docker-compose -f piggymetrics/docker-compose.custom.yml down
 done
--- run_all_scenarios_overhead_tracing.sh
+++ run_all_scenarios_overhead_tracing.sh
@@ -5,6 +5,9 @@ times=$1;
 mkdir -p overhead-experiments/tracing
 
 for (( i = 0; i < $times; i++ )); do
+
+	sudo systemctl start metricbeat.service
+
 	./run_scenario.sh -s 001_scenario -svc account --trace --latency
 	./run_scenario.sh -s 002_scenario -svc account --no-build --no-run --trace --latency
 	./run_scenario.sh -s 003_scenario -svc account --no-build --no-run --trace --latency
@@ -14,27 +17,29 @@ for (( i = 0; i < $times; i++ )); do
 	./run_scenario.sh -s 007_scenario -svc account --no-build --no-run --trace --latency
 	./run_scenario.sh -s 008_scenario -svc account --no-build --no-run --trace --latency
 
-	mkdir overhead-experiments/tracing/account-service/run-$i
-	cp scenarios/account-service/*.log overhead-experiments/tracing/account-service/run-$i
-	cp scenarios/account-service/*.pcap overhead-experiments/tracing/account-service/run-$i
+	mkdir -p overhead-experiments/tracing/account-service/run-$i
+	cp scenarios/account-service/*.log overhead-experiments/tracing/account-service/run-$i/
+	cp scenarios/account-service/*.pcap overhead-experiments/tracing/account-service/run-$i/
 
 	./run_scenario.sh -s 001_scenario -svc statistics --no-build --no-run --trace --latency
 	./run_scenario.sh -s 002_scenario -svc statistics --no-build --no-run --trace --latency
 	./run_scenario.sh -s 003_scenario -svc statistics --no-build --no-run --trace --latency
 	./run_scenario.sh -s 004_scenario -svc statistics --no-build --no-run --trace --latency
 
-	mkdir overhead-experiments/tracing/statistics-service/run-$i
-	cp scenarios/statistics-service/*.log overhead-experiments/tracing/statistics-service/run-$i
-	cp scenarios/statistics-service/*.pcap overhead-experiments/tracing/statistics-service/run-$i
+	mkdir -p overhead-experiments/tracing/statistics-service/run-$i
+	cp scenarios/statistics-service/*.log overhead-experiments/tracing/statistics-service/run-$i/
+	cp scenarios/statistics-service/*.pcap overhead-experiments/tracing/statistics-service/run-$i/
 
 	./run_scenario.sh -s 001_scenario -svc notification --no-build --no-run --trace --latency
 	./run_scenario.sh -s 002_scenario -svc notification --no-build --no-run --trace --latency
 	./run_scenario.sh -s 003_scenario -svc notification --no-build --no-run --trace --latency
 	./run_scenario.sh -s 004_scenario -svc notification --no-build --no-run --trace --latency
 
-	mkdir overhead-experiments/tracing/notification-service/run-$i
-	cp scenarios/notification-service/*.log overhead-experiments/tracing/notification-service/run-$i
-	cp scenarios/notification-service/*.pcap overhead-experiments/tracing/notification-service/run-$i
+	mkdir -p overhead-experiments/tracing/notification-service/run-$i
+	cp scenarios/notification-service/*.log overhead-experiments/tracing/notification-service/run-$i/
+	cp scenarios/notification-service/*.pcap overhead-experiments/tracing/notification-service/run-$i/
+
+	sudo systemctl stop metricbeat.service
 
 	docker-compose -f piggymetrics/docker-compose.custom.yml down
 done
