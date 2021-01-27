c7dc81807e9ede41ef53ad68a5a3d4d95f6f7590
Alessandro Tundo <alessandrotundo94@gmail.com>
2021-01-27 13:51:45 +0100

    Add missing export vars

--- run_all_scenarios_overhead_monitor.sh
+++ run_all_scenarios_overhead_monitor.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+export CONFIG_SERVICE_PASSWORD="conf_serv"
+export NOTIFICATION_SERVICE_PASSWORD="not_serv"
+export STATISTICS_SERVICE_PASSWORD="stat_serv"
+export ACCOUNT_SERVICE_PASSWORD="acc_serv"
+export MONGODB_PASSWORD="mongo"
+
 function copy_monitor_code() {
 	echo "Copying monitor instrumentation code..."
 
--- run_all_scenarios_overhead_tracing.sh
+++ run_all_scenarios_overhead_tracing.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+export CONFIG_SERVICE_PASSWORD="conf_serv"
+export NOTIFICATION_SERVICE_PASSWORD="not_serv"
+export STATISTICS_SERVICE_PASSWORD="stat_serv"
+export ACCOUNT_SERVICE_PASSWORD="acc_serv"
+export MONGODB_PASSWORD="mongo"
+
 function run_services() {
 	echo "Running services..."
 
